a9a983cf49cac06c53cebcce0b893e3e0cda16d6
Giang <nguyenvietgiang1110@gmail.com>
2025-01-05 15:04:32 +0700

    Validation and data response

--- src/main/java/vn/tayjava/controller/UserController.java
+++ src/main/java/vn/tayjava/controller/UserController.java
@@ -1,20 +1,80 @@
 package vn.tayjava.controller;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import vn.tayjava.dto.request.UserRequestDTO;
+import vn.tayjava.dto.response.ResponseFailure;
+import vn.tayjava.dto.response.ResponseSuccess;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
-@Tag(name = "Account", description = "Account management APIs")
+@Validated
 public class UserController {
-   @Operation(method = "POST", summary = "Add new user", description = "Send a request via this API to create new user")
+
     @PostMapping("/")
-    public String addUser(@RequestBody UserRequestDTO user) {
-        return "User added";
+    public ResponseSuccess addUser(@Valid @RequestBody UserRequestDTO user) {
+        System.out.println("Request add user " + user.getFirstName());
+
+        try {
+            return new ResponseSuccess(HttpStatus.CREATED, "User added successfully,", 1);
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseSuccess updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
+        System.out.println("Request update userId=" + userId);
+
+        try {
+            return new ResponseSuccess(HttpStatus.ACCEPTED, "User updated successfully");
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseSuccess updateStatus(@Min(1) @PathVariable int userId, @RequestParam boolean status) {
+        System.out.println("Request change status, userId=" + userId);
+
+        try {
+            return new ResponseSuccess(HttpStatus.ACCEPTED, "User's status changed successfully");
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseSuccess deleteUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int userId) {
+        System.out.println("Request delete userId=" + userId);
+
+        try {
+            return new ResponseSuccess(HttpStatus.NO_CONTENT, "User deleted successfully");
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseSuccess getUser(@PathVariable @Min(1) int userId) {
+        System.out.println("Request get user detail, userId=" + userId);
+
+        try {
+            return new ResponseSuccess(HttpStatus.OK, "user", new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789"));
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @GetMapping("/list")
+    public ResponseSuccess getAllUser(@RequestParam(defaultValue = "0", required = false) int pageNo,
+                                      @Min(10) @RequestParam(defaultValue = "20", required = false) int pageSize) {
+        System.out.println("Request get all of users");
+        return new ResponseSuccess(HttpStatus.OK, "users", List.of(new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789"),
+                new UserRequestDTO("Leo", "Messi", "leomessi@email.com", "0123456456")));
     }
 }

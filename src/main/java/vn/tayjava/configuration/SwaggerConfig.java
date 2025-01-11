bafa2e960a5519203bdbdcfd6d498dc504b261f0
Giang <nguyenvietgiang1110@gmail.com>
2025-01-12 01:37:47 +0700

    Login using JWT and spring security

--- src/main/java/vn/tayjava/configuration/SwaggerConfig.java
+++ src/main/java/vn/tayjava/configuration/SwaggerConfig.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,15 +17,12 @@ public class SwaggerConfig {
                         .title("GiangJava APIs")
                         .description("Spring Boot API sample with JWT authentication")
                         .version("1.0"))
-                .addSecurityItem(new SecurityRequirement().addList("Authorization"));
-    }
-
-    @Bean
-    public SecurityScheme securityScheme() {
-        return new SecurityScheme()
-                .name("Authorization")
-                .type(SecurityScheme.Type.APIKEY)
-                .in(SecurityScheme.In.HEADER)
-                .name(HttpHeaders.AUTHORIZATION);
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .name("Authorization")));
     }
 }

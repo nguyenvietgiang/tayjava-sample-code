996d1ece13842a2539a5fd72a1c16efca40d31c7
Giang <nguyenvietgiang1110@gmail.com>
2025-01-23 00:57:08 +0700

    grpc 30%

--- src/main/java/vn/tayjava/TayJavaApplication.java
+++ src/main/java/vn/tayjava/TayJavaApplication.java
@@ -1,16 +1,18 @@
 package vn.tayjava;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
 // cho phép chạy Background task
 @EnableScheduling
 public class TayJavaApplication {
-	// http://localhost:8000/swagger-ui/index.html
-	public static void main(String[] args) {
-		SpringApplication.run(TayJavaApplication.class, args);
-	}
+    // http://localhost:8000/swagger-ui/index.html
+    public static void main(String[] args) {
+        SpringApplication.run(TayJavaApplication.class, args);
+    }
 
 }
